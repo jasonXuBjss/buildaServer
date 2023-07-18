67ad6f28d7c2e6f908e0ea11fce2b9332cf6d194
jason.xu <Jason.Xu@bjss.com>
2023-07-18 22:59:23 +0100

    added env and gitignore

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 const Pool = require("pg").Pool;
+require('dotenv').config();
 
 const pool = new Pool ({
     user: "jasonxu",
-    password:"sepv3456",
+    password:process.env.DB_PASSWORD,
     host:"localhost",
     port: "5432",
     database:"bench_todo"
